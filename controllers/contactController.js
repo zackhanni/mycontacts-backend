2037389230f50505d601fe2f36e98f85ee30e383
Zack Hanni <zack.hannI@gmail.com>
2024-02-28 15:49:04 -0500

    Create contactController.js

---
+++ controllers/contactController.js
@@ -0,0 +1,48 @@
+//@desc Get all contacts
+//@route Get /api/contacts
+//@access public
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+};
+
+//@desc Create New contact
+//@route POST /api/contacts
+//@access public
+const createContact = (req, res) => {
+  console.log("The request body is:", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("Please fill in all fields.");
+  }
+  res.status(201).json({ message: "Create contact" });
+};
+
+//@desc Get contact
+//@route GET /api/contacts/:id
+//@access public
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get contact ${req.params.id}` });
+};
+
+//@desc Update contact
+//@route PUT /api/contacts/:id
+//@access public
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+};
+
+//@desc Delete contact
+//@route DELETE /api/contacts/:id
+//@access public
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: `Delete contact ${req.params.id}` });
+};
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
