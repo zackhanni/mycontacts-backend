a14114dd4915ce646237cb3fca76377cd19299b7
Zack Hanni <zack.hannI@gmail.com>
2024-02-28 15:50:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorhandler");
+
+const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
