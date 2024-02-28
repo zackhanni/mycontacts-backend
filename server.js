8ee7b2167fe5a45f6aeb7880d3f1f611812e4444
Zack Hanni <zack.hannI@gmail.com>
2024-02-28 16:53:04 -0500

    add connectDb to server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
+const connectDb = require("./config/dbConnection");
 
 const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
