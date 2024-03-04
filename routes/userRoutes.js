e60f81d388e066e6da299dcc491afaeff130a436
Zack Hanni <zack.hannI@gmail.com>
2024-03-04 16:28:42 -0500

    Create userRoutes.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,16 @@
+const {
+  registerUser,
+  loginUser,
+  currentUser,
+} = require("../controllers/userController");
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/current", currentUser);
+
+module.exports = router;
