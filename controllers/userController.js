23082b056cb9c5f7d94361bbc412e215effa8671
Zack Hanni <zack.hannI@gmail.com>
2024-03-05 13:25:41 -0500

    worked on loginUser api. added jsonwebtoken

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 //@desc Register a user
@@ -38,6 +39,31 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Email or password is not valid");
+  }
   res.json({ message: "Login user" });
 });
 
