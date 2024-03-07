90d9927e341746720adb938c3e1573e57603b94f
Zack Hanni <zack.hannI@gmail.com>
2024-03-07 15:55:21 -0500

    set req.user to the decoded user data

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -5,14 +5,20 @@ const validateToken = asyncHandler(async (req, res, next) => {
   let token;
   let authHeader = req.headers.Authorization || req.headers.authorization;
   if (authHeader && authHeader.startsWith("Bearer")) {
-    token = auth.split(" ")[1];
+    token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
         res.status(401);
         throw new Error("User is not authorized");
       }
-      console.log(decoded);
+      req.user = decoded.user;
+      next();
     });
+
+    if (!token) {
+      res.status(401);
+      throw newError("User is not authorized or token is missing");
+    }
   }
 });
 
