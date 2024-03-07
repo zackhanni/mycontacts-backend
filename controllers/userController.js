ed4f14405bad20e761cfb336bdc5142c4bff66b8
Zack Hanni <zack.hannI@gmail.com>
2024-03-07 15:54:41 -0500

    replace json response with the user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,7 +56,7 @@ const loginUser = asyncHandler(async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "1m" }
+      { expiresIn: "15m" }
     );
 
     res.status(200).json({ accessToken });
@@ -71,7 +71,7 @@ const loginUser = asyncHandler(async (req, res) => {
 //@route GET /api/current
 //@access private
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user information" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
